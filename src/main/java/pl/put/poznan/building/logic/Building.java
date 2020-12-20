09fdccb4721adb1f1e3def2ea194e19253079bff
JanCiechan <64535774+JanCiechan@users.noreply.github.com>
2020-12-20 15:50:36 +0100

    minor add functionality fix

--- src/main/java/pl/put/poznan/building/logic/Building.java
+++ src/main/java/pl/put/poznan/building/logic/Building.java
@@ -4,54 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Building extends Location {
-    private final List<Level> levelList;
+    public List<Level> levels;
     public Building(int id, String name,String type) {
         super(id, name,type);
-        levelList = new ArrayList<>();
+        levels = new ArrayList<>();
     }
 
     public void fillList(List<Level> addlevels){
-        levelList.clear();
+        levels.clear();
         for(Level item:addlevels){
             if(item.getBuildingid()==this.getId()){
-                levelList.add(item);
+                levels.add(item);
             }
         }
     }
     public float getArea(){
         float result=0;
-        for(Level item:levelList){
+        for(Level item: levels){
             result+=item.getArea();
         }
         return result;
     }
     public float getCubature(){
         float result=0;
-        for(Level item:levelList){
+        for(Level item: levels){
             result+=item.getCubature();
         }
         return result;
     }
     public float getLight(){
         float result=0;
-        for(Level item:levelList){
+        for(Level item: levels){
             result+=item.getLight();
         }
         return result;
     }
     public float getHeating(){
         float result=0;
-        for(Level item:levelList){
+        for(Level item: levels){
             result+=item.getHeating();
         }
         return result;
     }
     public int getAmountOfLevels(){
 
-        return getLevelList().size();
+        return getLevels().size();
     }
-    public List<Level> getLevelList(){
-        return levelList;
+    public List<Level> getLevels(){
+        return levels;
     }
 
 }
